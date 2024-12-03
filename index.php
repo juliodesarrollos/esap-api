c48722bfb0bcd2fa85fefa11c383dfcfa1101c27
juliodesarrollos <vqz.juliocesar@gmail.com>
2024-12-02 22:53:50 -0600

    se agregaron mas loggers para la insercion de usuarios

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
-require 'db.php';
 header("Content-Type: application/json");
 
 $url = $_SERVER['REQUEST_URI'];
-
-$db = Database::getInstance();
-$logger = new Log();
-$logger->write('Request received: ' . $_SERVER['REQUEST_METHOD']);
 
 if (strpos($url, '/usuarios') !== false) {
     require 'usuario.php';
