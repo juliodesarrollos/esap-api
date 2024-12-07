db542041546f4af1d69354547b0de3fbacfad27b
juliodesarrollos <vqz.juliocesar@gmail.com>
2024-12-07 13:18:46 -0600

    Se agrego rutas a index.php para manejar crud empresas

--- index.php
+++ index.php
@@ -20,10 +20,31 @@ if (strpos($url, '/login') !== false) {
             require 'delete_user.php';
             break;
         default:
+            http_response_code(405);
+            echo json_encode(['message' => 'Método no permitido']);
+            break;
+    }
+} elseif (strpos($url, '/empresa') !== false) {
+    switch ($_SERVER['REQUEST_METHOD']) {
+        case 'POST':
+            require 'create_empresa.php';
+            break;
+        case 'GET':
+            require 'read_empresa.php';
+            break;
+        case 'PUT':
+            require 'update_empresa.php';
+            break;
+        case 'DELETE':
+            require 'delete_empresa.php';
+            break;
+        default:
+            http_response_code(405);
             echo json_encode(['message' => 'Método no permitido']);
             break;
     }
 } else {
+    http_response_code(404);
     echo json_encode(['message' => 'Endpoint no encontrado']);
 }
 ?>
