183fed5e2d680ecc4d83ece077c0c2a6b351c466
juliodesarrollos <vqz.juliocesar@gmail.com>
2024-12-28 21:34:49 -0600

    get servicios by id empresa

--- services/read_servicio.php
+++ services/read_servicio.php
@@ -1,5 +1,5 @@
 <?php
-require 'db.php';
+require '../db.php';
 
 $db = Database::getInstance();
 $logger = new Log();
@@ -11,7 +11,12 @@ try {
         $id_servicio = $_GET['id_servicio'];
         $logger->write('Fetching servicio with ID: ' . $id_servicio);
 
-        $stmt = $db->prepare('SELECT * FROM servicio WHERE id_servicio = ?');
+        $stmt = $db->prepare('
+            SELECT * 
+            FROM servicio 
+            WHERE id_servicio = ?
+            ORDER BY id_servicio ASC
+        ');
         $stmt->execute([$id_servicio]);
         $servicio = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -23,10 +28,35 @@ try {
             http_response_code(404);
             echo json_encode(['message' => 'Servicio no encontrado']);
         }
+    } elseif (isset($_GET['id_empresa'])) {
+        $id_empresa = $_GET['id_empresa'];
+        $logger->write('Fetching servicios with empresa ID: ' . $id_empresa);
+
+        $stmt = $db->prepare('
+            SELECT * 
+            FROM servicio 
+            WHERE id_empresa = ?
+            ORDER BY id_servicio ASC
+        ');
+        $stmt->execute([$id_empresa]);
+        $servicios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($servicios) {
+            $logger->write('Servicios data fetched: ' . json_encode($servicios));
+            echo json_encode($servicios);
+        } else {
+            $logger->write('No servicios found with empresa ID: ' . $id_empresa);
+            http_response_code(404);
+            echo json_encode(['message' => 'No se encontraron servicios para la empresa especificada']);
+        }
     } else {
         $logger->write('Fetching all servicios');
 
-        $stmt = $db->query('SELECT * FROM servicio');
+        $stmt = $db->query('
+            SELECT * 
+            FROM servicio 
+            ORDER BY id_servicio ASC
+        ');
         $servicios = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
         $logger->write('All servicio data fetched: ' . json_encode($servicios));
