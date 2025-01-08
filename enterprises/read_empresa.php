5e4490cc4c82f898177aee8b8ed4558ff68fa6aa
juliodesarrollos <vqz.juliocesar@gmail.com>
2025-01-08 00:16:08 -0600

    get and create evaluacion_extintor by id servico/id_evaluacion

--- enterprises/read_empresa.php
+++ enterprises/read_empresa.php
@@ -10,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             SELECT e.*, u.id_usuario, u.nombre_usuario, u.direccion_usuario, u.telefono_usuario, u.correo_usuario, u.contraseña_usuario, u.tipo_usuario, u.first_login, u.created_at AS usuario_created_at, u.created_by AS usuario_created_by
             FROM empresa e
             LEFT JOIN usuario u ON e.id_empresa = u.id_empresa
-            WHERE u.id_usuario = (
+            AND u.id_usuario = (
                 SELECT MIN(id_usuario)
                 FROM usuario
                 WHERE id_empresa = e.id_empresa
@@ -22,18 +22,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         if ($empresas) {
             $result = [];
             foreach ($empresas as $empresa) {
-                $usuario = [
-                    'id_usuario' => $empresa['id_usuario'],
-                    'nombre_usuario' => $empresa['nombre_usuario'],
-                    'direccion_usuario' => $empresa['direccion_usuario'],
-                    'telefono_usuario' => $empresa['telefono_usuario'],
-                    'correo_usuario' => $empresa['correo_usuario'],
-                    'contraseña_usuario' => $empresa['contraseña_usuario'],
-                    'tipo_usuario' => $empresa['tipo_usuario'],
-                    'first_login' => $empresa['first_login'],
-                    'created_at' => $empresa['usuario_created_at'],
-                    'created_by' => $empresa['usuario_created_by']
-                ];
+                $usuario = null;
+                if ($empresa['id_usuario']) {
+                    $usuario = [
+                        'id_usuario' => $empresa['id_usuario'],
+                        'nombre_usuario' => $empresa['nombre_usuario'],
+                        'direccion_usuario' => $empresa['direccion_usuario'],
+                        'telefono_usuario' => $empresa['telefono_usuario'],
+                        'correo_usuario' => $empresa['correo_usuario'],
+                        'contraseña_usuario' => $empresa['contraseña_usuario'],
+                        'tipo_usuario' => $empresa['tipo_usuario'],
+                        'first_login' => $empresa['first_login'],
+                        'created_at' => $empresa['usuario_created_at'],
+                        'created_by' => $empresa['usuario_created_by']
+                    ];
+                }
                 unset($empresa['id_usuario'], $empresa['nombre_usuario'], $empresa['direccion_usuario'], $empresa['telefono_usuario'], $empresa['correo_usuario'], $empresa['contraseña_usuario'], $empresa['tipo_usuario'], $empresa['first_login'], $empresa['usuario_created_at'], $empresa['usuario_created_by']);
                 $empresa['usuario'] = $usuario;
                 $result[] = $empresa;
