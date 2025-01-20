64a746ab87d1f3ac8f2b3d9801210de3407f5783
juliodesarrollos <vqz.juliocesar@gmail.com>
2025-01-20 17:13:27 -0600

    get and create evaluacion_extintor by id servico/id_evaluacion

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $url = $_SERVER['REQUEST_URI'];
 if (strpos($url, '/login') !== false) {
     require 'login.php';
 } elseif(strpos($url, '/change_password') !== false) {
-    require 'change_password.php';
+    require 'users/change_password.php';
 } elseif (strpos($url, '/usuarios') !== false) {
     switch ($_SERVER['REQUEST_METHOD']) {
         case 'POST':
