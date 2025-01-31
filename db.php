f87031484a203dc119ea32c140eb555c6159cdd3
juliodesarrollos <vqz.juliocesar@gmail.com>
2025-01-30 20:16:30 -0600

    Update db.php for stg

--- db.php
+++ db.php
@@ -5,8 +5,8 @@ class Database {
     private static $instance = null;
     private $connection;
     private $host = 'localhost';
-    private $dbname = 'u943683090_esap_prod';
-    private $username = 'u943683090_esap_prod';
+    private $dbname = 'u943683090_esap_stg';
+    private $username = 'u943683090_esap_stg';
     private $password = 'E.s.a.p.1';
     private $logger;
 
